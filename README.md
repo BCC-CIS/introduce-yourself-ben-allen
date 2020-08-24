d14c50f3d0c8545645584fdd90395f0169c6c1b2
ben-allen-peralta <56449559+ben-allen-peralta@users.noreply.github.com>
2020-08-24 06:43:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# homework_0_cis6_cis25
+assignment 0 for cis6 and cis25
+
+To complete this assignment, please open the .cpp file and follow the instructions given in the comments
---
+++ first-c++-assignment.cpp
@@ -0,0 +1,27 @@
+/* file: first-c++-assignment.cpp
+ * ------------------------------
+ * An assignment for C++ classes, meant to verify that your
+ * development environment is working correctly.
+ * */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    // To complete this assignment, edit the text inside quotes on the three lines
+    // of code below this comment. Once done, click the green play arrow on the toolbar
+    // to compile and run your program. If it is working to your satisfaction, click
+    // VCS->commit to save the file to your github repository.
+    string name = "[put your name here]";
+    string pronouns = "[put your preferred pronouns here]";
+    string somethingSurprising = "[put something surprising about yourself here]";
+
+    cout << "name: " << name << endl;
+    cout << "preferred pronouns: " << pronouns << endl;
+    cout << "something about me: " << somethingSurprising << endl;
+
+    return 0;
+}
